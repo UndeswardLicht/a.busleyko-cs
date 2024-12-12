1d26135e2d9b1604bf806b928b4d8f188148cfcf
UndeswardLicht <AliaxejBusleikaV+github@gmail.com>
2024-12-12 23:13:20 +0100

    added unit 2 to project

---
+++ csharp_unit_selenium-master/ExampleProject/Selenium/BaseTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace ExampleProject
+{
+    internal class BaseTest
+    {
+        protected WebDriver driver;
+
+        protected static readonly string url = "https://the-internet.herokuapp.com/";
+        protected static readonly string preciseTextXpath = "//*[text()='{0}']";
+        protected static readonly string partialTextXpath = "//*[contains(text(),'{0}')]";
+        protected static readonly string relativePathFolder = @"Resources\"; //Path was fixed a little
+        protected WebDriverWait wait = null;
+        protected static readonly int maxWait = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(maxWait));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
