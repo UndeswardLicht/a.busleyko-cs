9fe47f40fdbbf4d339425f3c2f90fe391554da65
UndeswardLicht <AliaxejBusleikaV+github@gmail.com>
2024-12-13 17:34:31 +0100

    started task

---
+++ csharp_unit_selenium-master/ExampleProject/Selenium/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aquality.Selenium.Core;
+
+namespace ExampleProject.Selenium
+{
+    internal class Class1
+    {
+
+    }
+}
