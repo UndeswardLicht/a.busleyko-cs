1dbef77577c3e6b6805e6aacc5659b282e3bc9b6
UndeswardLicht <AliaxejBusleikaV+github@gmail.com>
2024-12-19 15:41:08 +0100

    added cookie test

---
+++ csharp_unit_selenium-master/ExampleProject/mytask/Tests/BaseCookieTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace ExampleProject.mytask.Tests
+{
+    internal class BaseCookieTest
+    {
+        protected WebDriver driver;
+        protected static readonly string url = "http://example.com/";
+        protected WebDriverWait wait = null;
+        protected static readonly int maxWait = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(maxWait));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
